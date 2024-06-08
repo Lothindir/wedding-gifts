3b33987aba25dab037f3ea2163f0e8b4fe0ca79d
Lothindir <lothindir@gmail.com>
2024-06-08 16:27:39 +0200

    feat: add localization change based on route

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createI18n } from 'vue-i18n'
 import ToastPlugin from 'vue-toast-notification'
 
 import 'vue-toast-notification/dist/theme-default.css'
@@ -14,8 +13,8 @@ import { i18n } from './utils/i18n'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 app.use(i18n)
+app.use(router)
 app.use(ToastPlugin)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { RouterView, createRouter, createWebHistory } from 'vue-router'
 import GiftsView from '@/views/GiftsView.vue'
+import { i18n } from '@/utils/i18n'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,22 @@ const router = createRouter({
       path: '/',
       name: 'gifts',
       component: GiftsView,
+    },
+    {
+      path: '/:lang',
+      component: GiftsView,
+      beforeEnter: (to) => {
+        const localeArray = i18n.global.availableLocales.map((l) => l.split('-')[0])
+        const futureLocale = to.params.lang.toString()
+
+        if (!localeArray.includes(futureLocale)) {
+          i18n.global.locale.value = i18n.global.fallbackLocale.value
+        } else {
+          const localeId = localeArray.findIndex((l) => l == futureLocale)
+          i18n.global.locale.value = i18n.global.availableLocales[localeId]
+        }
+        router.push('/')
+      },
     },
     {
       path: '/admin',
