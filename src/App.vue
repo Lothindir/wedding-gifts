d115c12c54e266acc5c8aea11339c0b08a487fd3
Lothindir <lothindir@gmail.com>
2024-06-18 13:18:49 +0200

    feat: restyle and add custom icons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, defineComponent, h } from 'vue'
 import { RouterView } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { i18n, loadLocaleMessages } from './utils/i18n';
 import { useSettingsStore } from '@/stores/settings'
+import { useGiftsStore } from '@/stores/gifts'
+import logoWhite from '@/assets/images/logo/logo-white_nt.svg'
 
-const { t } = useI18n()
+import FooterComponent from '@/components/FooterComponent.vue'
+
+const { t, locale } = useI18n()
 // const selectedLocale = ref<string>(i18n.global.locale)
 const settingsStore = useSettingsStore()
+const giftsStore = useGiftsStore()
 settingsStore.loadSettings()
+giftsStore.loadGifts()
+giftsStore.loadGiftsTranslations(locale.value)
 
 // async function changeLocale() {
 //   await loadLocaleMessages(i18n, selectedLocale.value)
@@ -23,7 +30,10 @@ settingsStore.loadSettings()
 
 <template>
   <header class="flex justify-between align-middle">
-    <h1 class="text-2xl font-bold p-4">{{ t('wedding.title') }} Katja & Francesco</h1>
+    <div class="flex align-baseline">
+      <img :src="logoWhite" class="my-1 ml-1 h-12 w-12" alt="Logo" />
+      <h1 class="text-2xl font-bold py-4 pl-1">{{ t('wedding.title') }} Katja & Francesco</h1>
+    </div>
     <div class="locale-changer mr-2 mt-2">
       <select id="locale" name="locale" v-model="$i18n.locale"
         class="my-auto mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
@@ -40,5 +50,10 @@ settingsStore.loadSettings()
       </select> -->
     </div>
   </header>
-  <RouterView />
+
+  <div class="h-full">
+    <RouterView class="flex-1" />
+  </div>
+
+  <FooterComponent />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+// const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     import('@tailwindcss/forms'),
+    import('@tailwindcss/aspect-ratio')
   ],
   variants: {
     extend: {
