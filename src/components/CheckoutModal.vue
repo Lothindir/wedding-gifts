9d5929cbc7e045e366a47c1a9b9ba7a3b797fae3
Lothindir <lothindir@gmail.com>
2024-08-09 13:19:23 +0200

    chore: lint file

--- src/components/CheckoutModal.vue
+++ src/components/CheckoutModal.vue
@@ -1,27 +1,43 @@
 <template>
   <TransitionRoot as="template" :show="open">
     <Dialog class="relative z-10" @close="close">
-      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
-        leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <TransitionChild as="template" enter="ease-out duration-300"
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
             enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
             leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
             <DialogPanel
-              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"
+            >
               <div>
-                <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                <div
+                  class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100"
+                >
                   <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
                 </div>
                 <div class="mt-3 text-center sm:mt-5">
-                  <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">{{
-                    t('checkout.modal_title') }}
+                  <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900"
+                    >{{ t('checkout.modal_title') }}
                   </DialogTitle>
                   <DialogDescription class="mt-2 text-sm text-gray-500">
                     {{ t('checkout.modal_description') }}
@@ -31,7 +47,9 @@
                   {{ t('checkout.modal_text') }}
                 </p>
               </div>
-              <div class="mt-6 border-t border-gray-200 text-gray-700 font-bold text-center text-xl pt-6">
+              <div
+                class="mt-6 border-t border-gray-200 text-gray-700 font-bold text-center text-xl pt-6"
+              >
                 {{ t('gift.amount_gifted') }} : {{ props.amountGifted }}
                 {{ settingsStore.currency }}
               </div>
@@ -75,9 +93,11 @@
               -->
 
               <div class="mt-5 sm:mt-6">
-                <button type="button"
+                <button
+                  type="button"
                   class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  @click="close">
+                  @click="close"
+                >
                   {{ t('utils.back') }}
                 </button>
               </div>
